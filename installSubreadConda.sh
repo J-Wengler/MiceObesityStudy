b47ac34506b31a3e980cc08a69525857bd44dfee
J-Wengler <jwengler@gmail.com>
2022-06-21 09:04:42 -0500

    Added documentation for all code

---
+++ installSubreadConda.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This installation requires Conda. For details on how to install/use Conda please refer to https://docs.conda.io/en/latest/
+
+# The below command installs the necessary package Subread (tested on Macbook Air i7 on June 21, 2022)
+conda install -c bioconda subread
